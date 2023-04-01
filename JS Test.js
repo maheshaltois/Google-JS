fd25a9fdc2208a1319d66db6e82775e016ab2626
Mahesh Mudaliyar <102156961+maheshaltois@users.noreply.github.com>
2023-04-01 13:52:20 +0530

    Update JS Test.js

--- JS Test.js
+++ JS Test.js
@@ -9,54 +9,114 @@ function dynamic_fields(module, module_fields, token) {
     if (schemas[module]) {
       return schemas[module];
     }
-
-    const fixedSchema = { schema: [], fields: [] };
-    const response = JSON.parse(UrlFetchApp.fetch(`https://api.pipedrive.com/v1/${module_fields}/?start=0&limit=500&api_token=${token}`));
-    const results = module === 'leads' ? response.data.filter(({ edit_flag }) => edit_flag) : response.data;
-    
-    if (module === 'leads') {
+    let fixedSchema = { schema: [], fields: [] };
+    let semantic_group, semantic_type, concept_type, data_type;
+    let datas = { "user": { "id": "NUMERIC", "name": "TEXT", "email": "TEXT", "active_flag": "BOOLEAN", "value": "NUMERIC" }, "person": { "active_flag": "BOOLEAN", "name": "TEXT", "email": "TEXT", "phone": "TEXT", "owner_id": "NUMERIC", "value": "NUMERIC" }, "people": { "active_flag": "BOOLEAN", "name": "TEXT", "email": "TEXT", "phone": "TEXT", "owner_id": "NUMERIC", "value": "NUMERIC" }, "org": { "name": "TEXT", "people_count": "NUMERIC", "owner_id": "NUMERIC", "address": "TEXT", "active_flag": "BOOLEAN", "cc_email": "TEXT", "value": "NUMERIC" }, "deal": { "name": "TEXT", "id": "NUMERIC" }, "lead": { "name": "TEXT", "id": "NUMERIC" } };
+    let semantics_data = { "all": { "semanticGroup": "TEXT" , "semanticType": "TEXT" , "conceptType": "DIMENSION" }, "date": { "semanticGroup": "DATE_TIME",  "semanticType": "YEAR_MONTH_DAY_SECOND",  "conceptType": "DIMENSION" }, "int": { "semanticGroup": "NUMERIC", "semanticType": "NUMBER", "conceptType": "METRIC" },"stage": { "semanticGroup": "NUMERIC", "semanticType": "NUMBER" , "conceptType": "METRIC" }, "double": { "semanticGroup": "NUMERIC", "semanticType": "NUMBER", "conceptType": "METRIC" }, "monetary": { "semanticGroup": "CURRENCY", "semanticType": "CURRENCY_INR", "conceptType": "METRIC" }, "boolean": {"semanticGroup": "BOOLEAN", "semanticType": "BOOLEAN", "conceptType": "METRIC" }, "picture": { "semanticGroup": "URL", "semanticType": "IMAGE", "conceptType": "DIMENSION" }}
+    let datatype_data = { "all": "STRING", "date": "STRING", "int": "NUMBER", "stage": "NUMBER", "double": "NUMBER", "monetary": "NUMBER", "boolean": "BOOLEAN", "picture": "STRING", }
+    let response = JSON.parse(UrlFetchApp.fetch(`https://api.pipedrive.com/v1/${module_fields}/?start=0&limit=500&api_token=${token}`));
+    let results = response.data;
+    if (module == 'leads') {
+      results = results.filter(({ edit_flag }) => edit_flag == true);
       fixedSchema['schema'] = leads_schema;
     }
+    results.forEach(function (field) {
+      let dataSchema = {
+        name: field.key,
+        label: field.name,
+        dataType: datatype_data[field.field_type] ? datatype_data[field.field_type] : datatype_data['all'],
+        fieldType: field.field_type,
+        options: field.options,
+        semantics: semantics_data[field.field_type] ? semantics_data[field.field_type] : semantics_data['all']
+      }
 
-    results.forEach(field => {
-      const dataSchema = processDataField(field, module_fields, token);
-      fixedSchema['schema'].push(dataSchema);
+      if (field.key == 'next_activity_date' || field.key == 'last_activity_date') {
+        let dataSchema_local = {
+          name: field.key,
+          label: field.name,
+          dataType: datatype_data[field.field_type] ? datatype_data[field.field_type] : datatype_data['all'],
+          fieldType: field.field_type,
+          options: field.options,
+          semantics: {
+            semanticType: 'YEAR_MONTH_DAY',
+            semanticGroup: 'DATE_TIME',
+            conceptType: 'DIMENSION',
+          }
+        }
+        fixedSchema['schema'].push(dataSchema_local);
+      }
+      if (field.key == 'pipeline' && module_fields == 'dealFields') {
+        let dataSchema_local = {
+          name: 'pipeline_id',
+          label: field.name,
+          dataType: 'NUMBER',
+          fieldType: field.field_type,
+          options: field.options,
+          semantics: {
+            semanticGroup: "NUMERIC", 
+            semanticType: "NUMBER", 
+            conceptType: "METRIC"
+          }
+        }
+        fixedSchema['schema'].push(dataSchema_local);
+      }
+      if (field.key == 'type' && module_fields == 'activityFields') {
+        let dataSchema_local = {
+          name: field.key,
+          label: field.name,
+          dataType: datatype_data[field.field_type] ? datatype_data[field.field_type] : datatype_data['all'],
+          fieldType: field.field_type,
+          options: attributeOptions(token, ['key_string', 'name'], 'activityTypes'),
+          semantics: {
+            semanticType: 'TEXT',
+            semanticGroup: 'TEXT',
+            conceptType: 'DIMENSION',
+          }
+        }
+        fixedSchema['schema'].push(dataSchema_local);
+      } else if (field.field_type === 'user' || field.field_type == 'person' || field.field_type == 'people' || field.field_type == 'org' || field.field_type == 'deal' || field.field_type == 'lead') {
+        let dataSchema_local;
+        Object.keys(datas[field.field_type]).forEach(function (data) {
+          switch (datas[field.field_type][data]) {
+            case 'NUMERIC':
+              semantic_group = 'NUMERIC';
+              semantic_type = 'NUMBER';
+              concept_type = 'METRIC'
+              data_type = 'NUMBER';
+              break;
+            case 'BOOLEAN':
+              semantic_group = 'BOOLEAN';
+              semantic_type = 'BOOLEAN';
+              concept_type = 'METRIC'
+              data_type = 'BOOLEAN';
+              break;
+            default:
+              semantic_group = 'TEXT';
+              semantic_type = 'TEXT';
+              concept_type = 'DIMENSION';
+              data_type = 'STRING';
+              break;
+          }
+          dataSchema_local = {
+            name: field.key + "." + data,
+            label: field.name + "_" + data,
+            dataType: data_type,
+            fieldType: field.field_type,
+            options: null,
+            semantics: {
+              conceptType: concept_type,
+              semanticType: semantic_type,
+              semanticGroup: semantic_group,
+            }
+          }
+          fixedSchema['schema'].push(dataSchema_local);
+        })
+      } else {
+        fixedSchema['schema'].push(dataSchema);
+      }
     });
-
     return fixedSchema;
   } catch (e) {
     console.error(e);
   }
-}
-
-function processDataField(field, module_fields, token) {
-  const datatype_data = { all: "STRING", date: "STRING", int: "NUMBER", stage: "NUMBER", double: "NUMBER", monetary: "NUMBER", boolean: "BOOLEAN", picture: "STRING" };
-  const dataType = datatype_data[field.field_type] || datatype_data['all'];
-  const semantics = generateSemantics(field, dataType);
-
-  let dataSchema = { name: field.key, label: field.name, dataType, fieldType: field.field_type, options: field.options, semantics };
-
-  if (field.key === 'next_activity_date' || field.key === 'last_activity_date') {
-    dataSchema.semantics = { semanticType: 'YEAR_MONTH_DAY', semanticGroup: 'DATE_TIME', conceptType: 'DIMENSION' };
-  }
-
-  if (field.key === 'pipeline' && module_fields === 'dealFields') {
-    dataSchema = { ...dataSchema, name: 'pipeline_id', dataType: 'NUMBER', semantics: semantics['NUMBER'] };
-  }
-
-  if (field.key === 'type' && module_fields === 'activityFields') {
-    dataSchema = { ...dataSchema, options: attributeOptions(token, ['key_string', 'name'], 'activityTypes'), semantics: semantics['STRING'] };
-  }
-
-  return dataSchema;
-}
-
-function generateSemantics(field, dataType) {
-  const semantics_data = {
-    STRING: { semanticGroup: "TEXT", semanticType: "TEXT", conceptType: "DIMENSION" },
-    NUMBER: { semanticGroup: "NUMERIC", semanticType: "NUMBER", conceptType: "METRIC" },
-    BOOLEAN: { semanticGroup: "BOOLEAN", semanticType: "BOOLEAN", conceptType: "METRIC" }
-  };
-
-  return semantics_data[dataType];
 }
